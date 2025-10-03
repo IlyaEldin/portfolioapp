0d4a85677867ee87e6bd1084aff7c3bb8d5e0787
Ilya <eldin.ilya@yandex.ru>
2025-10-03 17:37:00 +0300

    Сделал подвал

--- src/components/Footer/Footer.jsx
+++ src/components/Footer/Footer.jsx
@@ -1,5 +1,19 @@
 import classes from "./Footer.module.css";
 
 export default function Footer() {
-  return <div>Footer</div>;
+  return (
+    <>
+      <div className={classes.footerDivider}></div>
+      <div className={classes.footerContainer}>
+        <div className='logo'>IEldin</div>
+        <div className={classes.footerInfo}>
+          <p>
+            &copy; {new Date().getFullYear()} IEldin. Сайт создан с
+            использованием феймворка <span>ReactJS</span>
+          </p>
+          <p>Автор: Елдин Илья Михайлович</p>
+        </div>
+      </div>
+    </>
+  );
 }
--- src/components/HomePage/HomePage.jsx
+++ src/components/HomePage/HomePage.jsx
@@ -1,5 +1,15 @@
 import classes from "./HomePage.module.css";
+import ProductCard from "../ProductCard/ProductCard";
 
 export default function HomePage() {
-  return <div className={classes.contain}>HomePage</div>;
+  return (
+    <div className={classes.contain}>
+      <section className='Популярные товары'>
+        <h1>Пополуярыне товары</h1>
+        <div className='container-product-card'>
+          <ProductCard />
+        </div>
+      </section>
+    </div>
+  );
 }
