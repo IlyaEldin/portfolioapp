a728e7b532109651814aed612116ce84ea6aad9a
il4atechno <il4atechno>
2025-10-03 21:45:03 +0300

    Забрал монитор

--- src/components/HomePage/HomePage.jsx
+++ src/components/HomePage/HomePage.jsx
@@ -1,51 +1,14 @@
 import classes from "./HomePage.module.css";
 import ProductSliderSwiper from "../ProductSliderSwipe/ProductSliderSwiper";
+import { PRODUCTS } from "./products";
 
 export default function HomePage() {
-  const products = [
-    {
-      id: 1,
-      name: "Бананы",
-      price: 165,
-      units: "кг",
-      image: "/images/bananas.webp",
-    },
-    {
-      id: 2,
-      name: "ывапБананы",
-      price: 165,
-      units: "кг",
-      image: "/images/bananas.webp",
-    },
-    {
-      id: 3,
-      name: "ывапБананы",
-      price: 165,
-      units: "кг",
-      image: "/images/bananas.webp",
-    },
-    {
-      id: 4,
-      name: "ыввыБананы",
-      price: 165,
-      units: "кг",
-      image: "/images/bananas.webp",
-    },
-    {
-      id: 5,
-      name: "фываБананы",
-      price: 165,
-      units: "кг",
-      image: "/images/bananas.webp",
-    },
-  ];
-
   return (
     <div className={classes.contain}>
       <section className={classes.popularProductSection}>
         <h1>Пополуярные товары</h1>
         <div className={classes.containerProductCard}>
-          <ProductSliderSwiper products={products} />
+          <ProductSliderSwiper products={PRODUCTS} />
         </div>
       </section>
     </div>
---
+++ src/components/HomePage/products.js
@@ -0,0 +1,37 @@
+export const PRODUCTS = [
+  {
+    id: 1,
+    name: "Бананы",
+    price: 165,
+    units: "кг",
+    image: "/images/bananas.webp",
+  },
+  {
+    id: 2,
+    name: "ывапБананы",
+    price: 165,
+    units: "кг",
+    image: "/images/bananas.webp",
+  },
+  {
+    id: 3,
+    name: "ывапБананы",
+    price: 165,
+    units: "кг",
+    image: "/images/bananas.webp",
+  },
+  {
+    id: 4,
+    name: "ыввыБананы",
+    price: 165,
+    units: "кг",
+    image: "/images/bananas.webp",
+  },
+  {
+    id: 5,
+    name: "фываБананы",
+    price: 165,
+    units: "кг",
+    image: "/images/bananas.webp",
+  },
+];
