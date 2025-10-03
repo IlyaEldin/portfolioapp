4bea434a52e7a7ca551470eaebb5b882dd012baf
Ilya <eldin.ilya@yandex.ru>
2025-10-03 23:53:58 +0300

    Сделал слайдер

--- src/components/ProductSliderSwipe/ProductSliderSwiper.jsx
+++ src/components/ProductSliderSwipe/ProductSliderSwiper.jsx
@@ -3,9 +3,40 @@ import "swiper/css";
 import "swiper/css/navigation";
 import "swiper/css/pagination";
 import "./ProductSliderSwiper.css";
-import { useState } from "react";
+import { useEffect, useState } from "react";
 
 export default function ProductSliderSwiper({ products }) {
+  const [productId, setProductId] = useState([0, 3]);
+  // const [productReId, setProductReId] = useState([]);
+  const [productsVisible, setProductsVisible] = useState(
+    products.slice(productId[0], productId[1])
+  );
+
+  const handleSwipe = (type) => {
+    setProductId((prev) => {
+      const visibleCount = 3; // фиксированное количество карточек
+
+      let newStart = type === "left" ? prev[0] - 1 : prev[0] + 1;
+
+      // Простой круговой обработчик только для start
+      newStart = (newStart + products.length) % products.length;
+
+      return [newStart, newStart + visibleCount];
+    });
+  };
+
+  useEffect(() => {
+    const visibleCount = 3;
+    let visibleProducts = [];
+
+    for (let i = 0; i < visibleCount; i++) {
+      const index = (productId[0] + i) % products.length;
+      visibleProducts.push(products[index]);
+    }
+
+    setProductsVisible(visibleProducts);
+  }, [productId, products]);
+
   return (
     <div className='product-swiper'>
       <div className='btn-swiper'>
@@ -14,7 +45,7 @@ export default function ProductSliderSwiper({ products }) {
         </button>
       </div>
 
-      {products.map((product, index) => (
+      {productsVisible.map((product, index) => (
         <ProductCard key={`${product.id}-${index}`} product={product} />
       ))}
 
